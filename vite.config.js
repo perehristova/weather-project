ac1f442e1579774251a5557292859b83ee4790ad
Perekhristova <nas.prhv@gmail.com>
2026-02-18 00:41:49 +0700

    Add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,9 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+
+  base: '/weather-pj/',
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
